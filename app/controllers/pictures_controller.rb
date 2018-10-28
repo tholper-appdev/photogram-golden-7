26ac8177541929cf5df8f427444478ce5184a84d
Tim Holper <tholper@uchicago.edu>
2018-10-28 18:55:12 +0000

    photo_sun_2

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,7 +5,7 @@ class PicturesController < ApplicationController
         render("photos.html.erb")
     end
     def new_form
-        render("photos_new.html.erb")
+        render("photos_create_form.html.erb")
     end
     def create_row
         if params.fetch("the_source")
@@ -23,7 +23,7 @@ class PicturesController < ApplicationController
         photo.caption = @caption
         photo.save
 
-        render("photos_create.html.erb")
+        render("photos_create_action.html.erb")
     end  
     def destroy_row
         if params.fetch("id")
@@ -37,4 +37,57 @@ class PicturesController < ApplicationController
         
         render("photos_delete.html.erb")
     end
+    def edit_form
+        if params.fetch("id")
+            @id = params.fetch("id")
+        else
+            @id = ""
+        end 
+        
+        photo = Photo.find(@id)
+        @source = photo.source
+        @caption = photo.caption         
+        
+        render("photos_update_form.html.erb")
+    end
+    def update_row
+        if params.fetch("id")
+            @id = params.fetch("id")
+        else
+            @id = ""
+        end 
+        if params.fetch("the_source")
+            @source = params.fetch("the_source")
+        else
+            @source = ""
+        end
+        if params.fetch("the_caption")
+            @caption = params.fetch("the_caption")
+        else
+            @caption = ""
+        end        
+        
+        photo = Photo.find(@id)
+        photo.source = @source
+        photo.caption = @caption
+        photo.save
+
+        render("photos_update_action.html.erb")
+    end 
+    def show
+        if params.fetch("id")
+            @id = params.fetch("id")
+        else
+            @id = ""
+        end 
+        
+        photo = Photo.find(@id)
+        @source = photo.source
+        @caption = photo.caption
+
+        @created_at = photo.created_at
+        @minutes_ago = (Time.current - @created_at).minutes.round
+        
+        render("photos_detail.html.erb")
+    end  
 end
